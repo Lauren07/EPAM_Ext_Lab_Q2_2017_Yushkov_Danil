82511ac0cd4d4f20acafc73a9e61307dc86b5794
Danil <nydanil.rap@yandex.ru>
2017-08-08 18:59:10 +0400

    Added HWT_14 (JS topic)

---
+++ HWT_14/Task03/Task03.js
@@ -0,0 +1,10 @@
+﻿function buttonSelectorHandler(element, boxFrom, boxTo) {
+    var countChangedElements = 0;
+    $("#" + boxFrom + " option" + (element.value === '>' || element.value === '<' ? ":selected" : "")).each(function() {
+        $("#" + boxTo).append(new Option(this.text, this.value));
+        this.remove();
+        countChangedElements++;
+    });
+    if (countChangedElements===0)
+        alert("Нет доступных элементов для перемещения!");
+}
