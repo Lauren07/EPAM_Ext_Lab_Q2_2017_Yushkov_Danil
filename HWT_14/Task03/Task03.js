e41329ddc7a7fa992c149d6508fb9a04bb0ec869
Nikolay Piskarev <nikolay_piskarev@epam.com>
2017-08-22 09:13:50 +0400

    Задание 14 нужно доделать.

--- HWT_14/Task03/Task03.js
+++ HWT_14/Task03/Task03.js
@@ -1,4 +1,4 @@
-﻿function buttonSelectorHandler(element, boxFrom, boxTo) {
+function buttonSelectorHandler(element, boxFrom, boxTo) { //todo pn если будет несколько контролов, то работать будет только 1й.
     var countChangedElements = 0;
     $("#" + boxFrom + " option" + (element.value === '>' || element.value === '<' ? ":selected" : "")).each(function() {
         $("#" + boxTo).append(new Option(this.text, this.value));
